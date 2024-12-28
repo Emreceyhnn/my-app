32094bbea51f4e1701e7a34e2ce5ab88c174685e
Emreceyhnn <emreceyhnn@gmail.com>
2024-12-29 02:40:44 +0300

    asdasd

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { Stack, Typography } from "@mui/material";
+
+export default async function Page() {
+  return (
+    <>
+      <Typography>aksdasdaskdalskdm mala bak aq</Typography>
+    </>
+  );
+}
